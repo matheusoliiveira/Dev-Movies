b2549d898c4dfa943d6f189c3ebdb9ea12f7c6b3
Matheus Oliveira <matheus.felipee.oliveira@gmail.com>
2025-08-30 16:37:09 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // raiz do projeto onde está o index.html
+  publicDir: 'public', // se você tiver arquivos estáticos nessa pasta
+  build: {
+    outDir: 'dist', // pasta de saída do build
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'), // garante que o index.html seja lido corretamente
+    },
+  },
 })

b54587da57b9ea82b1a269e60417a407fff29914
Matheus Oliveira <matheus.felipee.oliveira@gmail.com>
2025-08-31 22:40:42 -0300

    Ajuste no vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './', // raiz do projeto onde está o index.html
-  publicDir: 'public', // se você tiver arquivos estáticos nessa pasta
+  publicDir: 'public', // ok se tiver assets estáticos
   build: {
-    outDir: 'dist', // pasta de saída do build
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), // garante que o index.html seja lido corretamente
-    },
+    outDir: 'dist', // padrão que o Render espera
   },
 })
